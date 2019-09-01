92d6c4861b4c8f286399fd8dfac6e6d55b0cc429
Administrator <18056033756@163.com>
2019-09-01 19:11:50 +0800

    aa

---
+++ src/main/java/com/hthyaq/zybadmin/service/impl/TablemapinfoCopyServiceImpl.java
@@ -0,0 +1,20 @@
+package com.hthyaq.zybadmin.service.impl;
+
+import com.hthyaq.zybadmin.model.entity.TablemapinfoCopy;
+import com.hthyaq.zybadmin.mapper.TablemapinfoCopyMapper;
+import com.hthyaq.zybadmin.service.TablemapinfoCopyService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 中英文表名、字段名的映射信息 服务实现类
+ * </p>
+ *
+ * @author zhangqiang
+ * @since 2019-09-01
+ */
+@Service
+public class TablemapinfoCopyServiceImpl extends ServiceImpl<TablemapinfoCopyMapper, TablemapinfoCopy> implements TablemapinfoCopyService {
+
+}
