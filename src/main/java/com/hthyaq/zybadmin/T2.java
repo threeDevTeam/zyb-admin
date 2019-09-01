99f974ae327428beec5ec1cc88f69f968f84dbe2
sq20190831 <sq20190831@163.com>
2019-09-01 11:25:40 +0800

    1

--- src/main/java/com/hthyaq/zybadmin/T2.java
+++ src/main/java/com/hthyaq/zybadmin/T2.java
@@ -1,7 +1,10 @@
 package com.hthyaq.zybadmin;
 
 public class T2 {
-    int i;
+
+    public void c(){
+        System.out.println("a");
+    }
     public void a(){
         System.out.println("a");
     }
