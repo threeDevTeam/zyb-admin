f3377ce5074124f77a6ee2114a58727592e7d6b5
Administrator <18056033756@163.com>
2019-09-10 19:23:09 +0800

    aa

--- src/main/java/com/hthyaq/zybadmin/controller/ZhenduanDetailOfServiceController.java
+++ src/main/java/com/hthyaq/zybadmin/controller/ZhenduanDetailOfServiceController.java
@@ -1,9 +1,20 @@
 package com.hthyaq.zybadmin.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.google.common.base.Strings;
+import com.hthyaq.zybadmin.model.entity.ZhenduanDetailOfService;
+import com.hthyaq.zybadmin.model.entity.ZhenduanTotalOfService;
+import com.hthyaq.zybadmin.service.ZhenduanDetailOfServiceService;
+import com.hthyaq.zybadmin.service.ZhenduanTotalOfServiceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +27,56 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/zhenduanDetailOfService")
 public class ZhenduanDetailOfServiceController {
+    @Autowired
+    ZhenduanDetailOfServiceService zhenduanDetailOfServiceService;
 
+    @PostMapping("/add")
+    public boolean add(@RequestBody ZhenduanDetailOfService zhenduanDetailOfService) {
+        return zhenduanDetailOfServiceService.save(zhenduanDetailOfService);
+    }
+
+    @GetMapping("/delete")
+    public boolean delete(String id) {
+        return zhenduanDetailOfServiceService.removeById(id);
+    }
+
+    @GetMapping("/getById")
+    public ZhenduanDetailOfService getById(Integer id) {
+
+        ZhenduanDetailOfService zhenduanDetailOfService = zhenduanDetailOfServiceService.getById(id);
+        //demoCourse
+        QueryWrapper<ZhenduanDetailOfService> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("id", id);
+        List<ZhenduanDetailOfService> demoCourseList = zhenduanDetailOfServiceService.list(queryWrapper);
+        //将demoCourse的数据设置到demoData
+        return zhenduanDetailOfService;
+    }
+
+    @PostMapping("/edit")
+    public boolean edit(@RequestBody ZhenduanDetailOfService zhenduanDetailOfService) {
+        return zhenduanDetailOfServiceService.updateById(zhenduanDetailOfService);
+    }
+
+    @GetMapping("/list")
+    public IPage<ZhenduanDetailOfService> list(String json) {
+        //字符串解析成java对象
+        JSONObject jsonObject = JSON.parseObject(json);
+        //从对象中获取值
+        Integer currentPage = jsonObject.getInteger("currentPage");
+        Integer pageSize = jsonObject.getInteger("pageSize");
+        String name = jsonObject.getString("name");
+        String idNum = jsonObject.getString("idNum");
+        QueryWrapper<ZhenduanDetailOfService> queryWrapper = new QueryWrapper<>();
+        if (!Strings.isNullOrEmpty(name)) {
+            queryWrapper.eq("year", name);
+        }
+        if (!Strings.isNullOrEmpty(idNum)) {
+            queryWrapper.eq("year", idNum);
+        }
+
+        IPage<ZhenduanDetailOfService> page = zhenduanDetailOfServiceService.page(new Page<>(currentPage, pageSize), queryWrapper);
+
+        return page;
+    }
 }
+
