63d80fe0fdc8979910816346aa89f22daa5aed83
jonny201251 <jonny201251@163.com>
2019-09-26 17:17:00 +0800

    登录拦截器

--- src/main/java/com/hthyaq/zybadmin/controller/SysUserController.java
+++ src/main/java/com/hthyaq/zybadmin/controller/SysUserController.java
@@ -3,18 +3,13 @@ package com.hthyaq.zybadmin.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.hthyaq.zybadmin.common.constants.GlobalConstants;
-import com.hthyaq.zybadmin.model.entity.EnterpriseOfRegister;
-import com.hthyaq.zybadmin.model.entity.MailBean;
+import com.hthyaq.zybadmin.model.bean.GlobalResult;
 import com.hthyaq.zybadmin.model.entity.SysUser;
-import com.hthyaq.zybadmin.service.EnterpriseOfRegisterService;
 import com.hthyaq.zybadmin.service.SysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
@@ -41,18 +36,14 @@ public class SysUserController {
     }
 
     @PostMapping("/register")
-    public boolean register(@RequestBody SysUser sysUser, HttpSession httpSession) {
-        List<SysUser> list = sysUserService.list();
-        boolean flag = false;
-        for (SysUser user : list) {
-            if (sysUser.getLoginName().equals(user.getLoginName())
-                    && sysUser.getLoginPassword().equals(user.getLoginPassword())
-            ) {
-                flag=true;
-                httpSession.setAttribute(GlobalConstants.LOGIN_NAME,user);
-            }
+    public GlobalResult register(@RequestBody SysUser sysUserForm, HttpSession httpSession) {
+        List<SysUser> list = sysUserService.list(new QueryWrapper<SysUser>().eq("loginName", sysUserForm.getLoginName()).eq("loginPassword", sysUserForm.getLoginPassword()));
+        if (list.size() != 1) {
+            return GlobalResult.fail();
         }
-        return flag;
+        SysUser sysUserDb = list.get(0);
+        httpSession.setAttribute(GlobalConstants.LOGIN_NAME, sysUserDb);
+        return GlobalResult.success("", sysUserDb);
     }
 }
 
