2dd00789611d14e274fc1fa886ce189d6828c57c
jonny201251 <jonny201251@163.com>
2019-11-21 15:20:54 +0800

    更新

--- src/main/java/com/hthyaq/zybadmin/controller/IndexController.java
+++ src/main/java/com/hthyaq/zybadmin/controller/IndexController.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-    @GetMapping({"/visual/**","/user/**", "/*supervise", "/*Supervise", "/*Service", "/*enterprise", "/*Enterprise","/changePassword","/otherOfDic","/*Management", "/dataVisualSwitchFlag"})
+    @GetMapping({"/","/visual/**","/user/**", "/*supervise", "/*Supervise", "/*Service", "/*enterprise", "/*Enterprise","/changePassword","/otherOfDic","/*Management", "/dataVisualSwitchFlag"})
     public String index() {
+        System.out.println("index");
         return "index";
     }
 
