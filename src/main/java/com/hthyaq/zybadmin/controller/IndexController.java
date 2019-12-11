177fd277c2a952338d6020c41a6ebe4f04a45e8a
jonny201251 <jonny201251@163.com>
2019-12-11 11:32:12 +0800

    更新

--- src/main/java/com/hthyaq/zybadmin/controller/IndexController.java
+++ src/main/java/com/hthyaq/zybadmin/controller/IndexController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class IndexController {
-//    @GetMapping({"/","/visual/**","/user/**", "/*supervise", "/*Supervise", "/*Service", "/*enterprise", "/*Enterprise","/*Management"})
+//    @GetMapping({"/","/visual/**","/user/**", "/*supervise", "/*Supervise", "/*Service", "/*enterprise", "/*Enterprise","/*Management","/opinion*"})
     public String index() {
         System.out.println("index");
         return "index";
